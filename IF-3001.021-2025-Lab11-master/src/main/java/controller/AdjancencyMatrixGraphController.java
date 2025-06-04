69ed8bde064cf8e642f020046b008d9784cb9e3b
Jimena Calvo <jimenacalvosoto13@gmail.com>
2025-06-04 17:45:44 -0600

    Parte grafica completa

---
+++ IF-3001.021-2025-Lab11-master/src/main/java/controller/AdjancencyMatrixGraphController.java
@@ -0,0 +1,54 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+
+public class AdjancencyMatrixGraphController {
+    @javafx.fxml.FXML
+    private RadioButton avlRadioButton;
+    @javafx.fxml.FXML
+    private Pane mainPain;
+    @javafx.fxml.FXML
+    private Pane pane2;
+    @javafx.fxml.FXML
+    private RadioButton bstRadioButton;
+    @javafx.fxml.FXML
+    private ToggleGroup treeTypeToggleGroup;
+    @javafx.fxml.FXML
+    private Label balanceLabel;
+    @javafx.fxml.FXML
+    private AnchorPane AP;
+
+    @javafx.fxml.FXML
+    public void nodeHeightOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void removeOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void addOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void randomizeOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void containsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void treeHeightOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleScrollZoom(Event event) {
+    }
+}
---
+++ IF-3001.021-2025-Lab11-master/src/main/java/controller/SinglyLinkedListGraph.java
@@ -0,0 +1,46 @@
+package controller;
+
+public class SinglyLinkedListGraph {
+    @javafx.fxml.FXML
+    private RadioButton avlRadioButton;
+    @javafx.fxml.FXML
+    private Pane mainPain;
+    @javafx.fxml.FXML
+    private Pane pane2;
+    @javafx.fxml.FXML
+    private RadioButton bstRadioButton;
+    @javafx.fxml.FXML
+    private ToggleGroup treeTypeToggleGroup;
+    @javafx.fxml.FXML
+    private Label balanceLabel;
+    @javafx.fxml.FXML
+    private AnchorPane AP;
+
+    @javafx.fxml.FXML
+    public void nodeHeightOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void removeOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void addOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void randomizeOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void containsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void treeHeightOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void handleScrollZoom(Event event) {
+    }
+}
